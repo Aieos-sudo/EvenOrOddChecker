81c97b7534833fdc7ada8704ac3513feb6abf9c2
Aieos <firered9000@yahoo.com>
2021-11-18 23:53:21 -0600

    initial commit

---
+++ EvenorOddChecker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EvenorOddChecker
+{
+    internal class Program
+    {
+        public static string OddEvenChecker(string number)
+        {
+            int intnumber = Convert.ToInt32(number);
+            if (intnumber % 2 == 1)
+                return "odd";
+            else
+                return "even:";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Even or Odd Checker\n");
+            Console.Write("Enter an integer: ");
+            string number = Console.ReadLine();
+            string evenOrOdd = OddEvenChecker(number);
+            Console.WriteLine("This is an " + evenOrOdd +" number");
+        }
+    }
+}
