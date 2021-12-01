1b12d784eb5a0087fd9c31b40a5d1c980f70ef82
Aieos <firered9000@yahoo.com>
2021-11-30 22:00:48 -0600

    added exception handling

--- EvenorOddChecker/Program.cs
+++ EvenorOddChecker/Program.cs
@@ -6,19 +6,44 @@ namespace EvenorOddChecker
     {
         public static string OddEvenChecker(string number)
         {
-            int intnumber = Convert.ToInt32(number);
-            if (intnumber % 2 == 1)
-                return "odd";
-            else
-                return "even:";
+            string result = "";
+            try
+            {
+                int intnumber = Convert.ToInt32(number);
+                if (intnumber % 2 == 1)
+                    result = "odd";
+                else
+                    result = "even";
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Entered value must be an integer. Error: " + ex.Message);
+                Console.WriteLine();
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Entered value was too large. Please enter a value less than 2.4 billion. Error: " + ex.Message);
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong. Error: " + ex.Message);
+                Console.WriteLine();
+            }
+            return result;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Even or Odd Checker\n");
-            Console.Write("Enter an integer: ");
-            string number = Console.ReadLine();
-            string evenOrOdd = OddEvenChecker(number);
-            Console.WriteLine("This is an " + evenOrOdd +" number");
+            string evenOrOdd;
+            do
+            {
+                Console.WriteLine("Even or Odd Checker\n");
+                Console.Write("Enter an integer: ");
+                string number = Console.ReadLine();
+                evenOrOdd = OddEvenChecker(number);
+            }
+            while (evenOrOdd == "");
+            Console.WriteLine("This is an " + evenOrOdd + " number");
         }
     }
 }
